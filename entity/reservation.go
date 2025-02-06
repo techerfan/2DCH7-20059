47ea9ffa7f72df26dab46c7ff6cb3d503d41ab79
Erfan Derakhshani <techerfan@gmail.com>
2025-02-06 08:53:40 +0000

    Fix: EndDT type must be time.Time instead of time.Duration

--- entity/reservation.go
+++ entity/reservation.go
@@ -3,13 +3,13 @@ package entity
 import "time"
 
 type Reservation struct {
-	ID            uint          `json:"id"`
-	NumberOfSeats uint8         `json:"number_of_seats"`
-	UserID        uint          `json:"user_id"`
-	TableID       uint          `json:"table_id"`
-	ReceiptID     uint          `json:"receipt_id"`
-	StartDT       time.Time     `json:"start_dt"`
-	EndDT         time.Duration `json:"end_dt"`
+	ID            uint      `json:"id"`
+	NumberOfSeats uint8     `json:"number_of_seats"`
+	UserID        uint      `json:"user_id"`
+	TableID       uint      `json:"table_id"`
+	ReceiptID     uint      `json:"receipt_id"`
+	StartDT       time.Time `json:"start_dt"`
+	EndDT         time.Time `json:"end_dt"`
 	// The cancelation is only valid when the client has payed the receipt (Otherwise, the reservation is not submitted)
 	// * Also, if the client wants to cancel the reservation, they must do it 3 hours before the start time
 	IsCanceled bool `json:"is_canceled"`
