ddeed63e70c13255d5e9c2349604939a8dfba538
Erfan Derakhshani <techerfan@gmail.com>
2025-02-06 14:09:29 +0000

    Feat: discovered more requirements and added them to user service interface

--- contract/user_service.go
+++ contract/user_service.go
@@ -6,7 +6,13 @@ import (
 	"github.com/techerfan/2DCH7-20059/dto"
 )
 
+type CtxKey int
+
+const UserID CtxKey = iota + 1
+
 type UserService interface {
+	IsTokenValid(context.Context, string) bool
 	Register(context.Context, dto.UserRegisterRequest) (dto.UserRegisterResponse, error)
-	Login(context.Context, dto.UserLoginRequest) (dto.UserLoginRequest, error)
+	Login(context.Context, dto.UserLoginRequest) (dto.UserLoginResponse, error)
+	Logout(context.Context, dto.UserLogoutRequest) (dto.UserLogoutResponse, error)
 }
