b57198f433d141741f342fbd79b081f3876addb0
Erfan Derakhshani <techerfan@gmail.com>
2025-02-07 08:12:24 +0000

    Fix: modfied type of the end dt

--- dto/reservation.go
+++ dto/reservation.go
@@ -3,12 +3,12 @@ package dto
 import "time"
 
 type Reservation struct {
-	ID            uint          `json:"id"`
-	NumberOfSeats uint8         `json:"number_of_seats"`
-	UserID        uint          `json:"user_id"`
-	TableID       uint          `json:"table_id"`
-	ReceiptID     uint          `json:"receipt_id"`
-	StartDT       time.Time     `json:"start_dt"`
-	EndDT         time.Duration `json:"end_dt"`
-	IsCanceled    bool          `json:"is_canceled"`
+	ID            uint      `json:"id"`
+	NumberOfSeats uint8     `json:"number_of_seats"`
+	UserID        uint      `json:"user_id"`
+	TableID       uint      `json:"table_id"`
+	ReceiptID     uint      `json:"receipt_id"`
+	StartDT       time.Time `json:"start_dt"`
+	EndDT         time.Time `json:"end_dt"`
+	IsCanceled    bool      `json:"is_canceled"`
 }
